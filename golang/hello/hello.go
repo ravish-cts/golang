cf7a97c63434caf615698495e5b82e1f07aa4997
ravish.tayal <ravish.tayal@cognizant.com>
2020-03-25 10:46:46 +0000

    inital repo creation for golang work

---
+++ golang/hello/hello.go
@@ -0,0 +1,37 @@
+package main 
+
+import (
+	"fmt"
+	"os"
+	"time" 
+	"errors"
+) 
+
+func getGreeting(timeNow int)(string,error){
+ var message string
+ if timeNow < 7	{
+	 message = "To early time"
+	 err := errors.New("To early time")
+	 return message,err	
+	}else {
+	 message = "Good Day" 
+	} 
+	 return message,nil	
+}
+func main() {
+	var timeNow int; 
+	if len(os.Args) > 1{
+	fmt.Println(os.Args[1]);
+	} else {
+	fmt.Println("Hello, my first hello from Golang !");
+	}
+	timeNow = time.Now().Hour();
+        Msg,err := getGreeting(timeNow)
+	if err != nil {
+	  Msg = "This is too early !"
+	  os.Exit(1); 
+	}else {
+	 fmt.Println("Wait for greeting");
+	} 
+	fmt.Println(Msg);
+}
